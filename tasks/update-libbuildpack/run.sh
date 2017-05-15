9dc2425122d93891861073bbe59ab0e2b29ff66f
Dave Goddard <dave@goddard.id.au>
2017-05-15 08:33:07 -0400

    Run go generate on hooks directory
    (Libbuildpack)
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- tasks/update-libbuildpack/run.sh
+++ tasks/update-libbuildpack/run.sh
@@ -27,6 +27,7 @@ pushd buildpack
 		go get github.com/onsi/gomega
 
 		go generate || true
+		[ -d hooks ] && (cd hooks && (go generate || true))
 		[ -d supply ] && (cd supply && (go generate || true))
 		[ -d finalize ] && (cd finalize && (go generate || true))
 		ginkgo -r
